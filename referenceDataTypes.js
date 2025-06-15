e54136bd043fa47d87728d766b4b334f7a0a75ac
Alisha <aarya.alisha30@gmail.com>
2025-06-15 18:28:01 +0530

    files added

---
+++ referenceDataTypes.js
@@ -0,0 +1,18 @@
+ //Object in JS
+
+ var myInfo = {
+    name: "Abhay", 
+    age: 23, 
+    mob: 9687865845, 
+    eMail: "abhay123@gmail.com",
+    course: "Btech",
+    add: "Gzb",
+    isMarried: false
+}
+// how to access object's keys........
+
+// using dot notation or bracket
+
+console.log(myInfo.name); // Abhay
+console.log(myInfo['mob']); //9687865845
+        
